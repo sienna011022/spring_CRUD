18e1ee0895251b3b33b4a2ca559450100dd9e206
sienna <20200803@sungshin.ac.kr>
2022-06-21 23:05:49 +0900

    스프링 뷰 연결:

---
+++ src/main/java/com/example/projectCRUD/ProjectCrudApplication.java
@@ -0,0 +1,14 @@
+package com.example.projectCRUD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+public class ProjectCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectCrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/projectCRUD/controller/FirstController.java
@@ -0,0 +1,17 @@
+package com.example.projectCRUD.controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class FirstController {
+    @GetMapping("/greetings")
+    public String niceToMeetYou(Model model){
+        model.addAttribute("username","sienna");
+        //파일 이름만 return 로
+        //templates/greetings.mustache 를 찾아 브라우저로 전송
+        return "greetings";
+    }
+}
